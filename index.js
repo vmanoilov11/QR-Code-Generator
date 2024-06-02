2ef0ed4fe993518de7b5ab76fc6b51251eddf5dc
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-06-02 16:17:13 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+import qr from "qr-image";
+import fs from "fs";
+
+
+inquirer
+  .prompt([
+    {
+        "message": "Type your message here: ",
+        "name": "URL"
+    }
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    let qr_res = qr.image(url);
+    qr_res.pipe(fs.createWriteStream('qr_img.svg'));
+
+    
+    fs.writeFile('URL.txt', url, (err) => {
+        if(err) throw err;
+        console.log("File saved sucsesfullu")
+    })
+        
+
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
