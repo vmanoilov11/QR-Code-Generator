660df4c15848ba8fa611eba28f2ac71c767a25d3
Vladislav Manoilov <162327088+vmanoilov11@users.noreply.github.com>
2024-06-02 16:17:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ inquirer
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
     } else {
-      // Something else went wrong
     }
   });
